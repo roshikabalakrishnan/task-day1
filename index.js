69118339605c7c1ff7ace42c118930bc77a3b6f7
roshikabalakrishnan <roshikabalakrishnan108@gmail.com>
2025-07-12 10:46:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var number=12;
+var num=12;
 var name='js';
 var iscloudy=true;
-console.log(number+" "+name+" "+iscloudy);
-document.write(number);
+console.log(num+" "+name+" "+iscloudy);
+document.write(num);
 document.write(name);
-Document.write(iscloudy);
+document.write(iscloudy);
+
+//task2
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+
+//task3
+ var num1=parseInt(prompt("enter the first value"));
+ var num2=parseInt(prompt("enter the second value"));
+ alert(num1+num2);
+ console.log(num1+num2);
+ console.log(num1-num2);
+ console.log(num1*num);
+ console.log(num1/num2);
+
